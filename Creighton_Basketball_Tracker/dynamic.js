5d6d48e2d292a95d0b7295fbfc700054fff3f8eb
lucasjohnson7 <111755351+lucasjohnson7@users.noreply.github.com>
2023-12-04 15:09:01 -0600

    Update dynamic.js
    added table ordering but not functioning correctly

--- Creighton_Basketball_Tracker/dynamic.js
+++ Creighton_Basketball_Tracker/dynamic.js
@@ -99,6 +99,26 @@ const scheduleData = {
     // Add more years as needed
 };
 
+
+
+//new Code added for ordering tables
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+// do the work...
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => table.appendChild(tr));
+
+})));
+
+
+
 function populateData(year) {
     const statsTableBody = document.querySelector("#stats-table tbody");
     const winningSeasonsList = document.querySelector("#winning-seasons-list");
